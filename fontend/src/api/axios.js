24d43110c22213efc1991a62a126fcacc67344d1
joy <joyr43004@gmail.com>
2026-05-11 20:13:46 +0600

    backend server url update

--- fontend/src/api/axios.js
+++ fontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios  from "axios";
 
 
 const axiosInstance=axios.create({
-    baseURL:"https://ecommerce-project-w4gu.onrender.com",
+    baseURL:"https://ecommerce-project-w4gu.onrender.com/",
     headers:{
       "Content-Type": "application/json",
     },
