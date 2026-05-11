a25997279831594e5e1e3e9fd1f549cc75cd3b8c
joy <joyr43004@gmail.com>
2026-05-11 20:25:41 +0600

    cors-allow-origins fontend url set

--- backend/mixin/settings.py
+++ backend/mixin/settings.py
@@ -79,7 +79,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ORIGINS = [
+    "https://ecommerce-project-dtot.vercel.app"
+]
 
 ROOT_URLCONF = 'mixin.urls'
 
