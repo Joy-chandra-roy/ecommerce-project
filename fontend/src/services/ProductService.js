160a8328013c18787affe2154e0d82441829e05c
joy <joyr43004@gmail.com>
2026-05-11 20:47:05 +0600

    fontend product service file update

--- fontend/src/services/ProductService.js
+++ fontend/src/services/ProductService.js
@@ -2,6 +2,7 @@ import axiosInstance from './../api/axios';
 
 const getProducts=(page=1, search="", ordering="")=>{
     return axiosInstance.get(`/products/?page=${page}&search=${search}&ordering=${ordering}`);
+   
 };
 
 const createProduct=(data)=>{
@@ -9,11 +10,11 @@ const createProduct=(data)=>{
 };
 
 const updateProduct=(id, data)=>{
-    return axiosInstance.put(`/products/${id}`, data);
+    return axiosInstance.put(`/products/${id}/`, data);
 }
 
 const deleteProduct=(id)=>{
-    return axiosInstance.delete(`/products/${id}`)
+    return axiosInstance.delete(`/products/${id}/`)
 }
 
 
