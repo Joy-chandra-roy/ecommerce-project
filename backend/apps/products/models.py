f04f0cbffd184980876aa33ed268c1a2378899eb
joy <joyr43004@gmail.com>
2026-05-17 22:23:44 +0600

    backend product models image field update

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name=models.CharField(max_length=100)
     price=models.DecimalField(max_digits=10, decimal_places=2)
     description=models.TextField(blank=True, null=True)
-    image=CloudinaryField('image')
+    image=models.ImageField(upload_to="image")
     
     is_available=models.BooleanField(default=True)
     is_featured=models.BooleanField(default=False)
