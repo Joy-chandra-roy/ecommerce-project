c566f1c4a81370fc96ada0757b0d84e1a658d473
joy <joyr43004@gmail.com>
2026-05-20 02:13:38 +0600

    backend product serializer update

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-
 from apps.products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    
+    
     class Meta:
         model=Product
         fields="__all__"
+        
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
